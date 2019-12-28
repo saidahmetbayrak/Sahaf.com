2a2fa94f141da190887c6c62658bae5a8ed37e58
Burak Akça <56919004+burakakca@users.noreply.github.com>
2019-12-28 19:52:19 +0300

    UI.MVC Eklendi
    ui eklendi , admin paneli hariç !
    şuanlık dokunmayın ui'a sorusu olan sorsun wp'den ---- BURAK

---
+++ Sahaf.UI.MVC/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sahaf.UI.MVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
