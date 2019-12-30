1baafa7891852ddb839248c172e29ff705ea78b3
Burak Akça <56919004+burakakca@users.noreply.github.com>
2019-12-30 10:13:50 +0300

    Create MyStrategy.cs

---
+++ Sahaf.DAL/MyStrategy.cs
@@ -0,0 +1,14 @@
+﻿using Sahaf.DAL.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sahaf.DAL
+{
+    class MyStrategy : DropCreateDatabaseIfModelChanges<SahafDbContext>
+    {
+    }
+}
